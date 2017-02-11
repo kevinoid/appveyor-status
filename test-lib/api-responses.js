fce5bbc3d36addaa45e54bc80c4c8806f7b9d8b6
Kevin Locke <kevin@kevinlocke.name>
2017-02-10 19:23:16 -0700

    Add tests for appveyorUtils
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ test-lib/api-responses.js
@@ -0,0 +1,181 @@
+/**
+ * @copyright Copyright 2017 Test Author <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var badgeData;
+
+/** Gets a ProjectBuild API response with a given status.
+ */
+exports.getProjectBuild = function getLastBuild(status) {
+  return {
+    project: {
+      projectId: 12345,
+      accountId: 6789,
+      accountName: 'test-account-name',
+      builds: [],
+      name: 'Test Project',
+      slug: 'test-proj',
+      repositoryType: 'gitHub',
+      repositoryScm: 'git',
+      repositoryName: 'test-account-name/test-proj',
+      repositoryBranch: 'master',
+      isPrivate: false,
+      skipBranchesWithoutAppveyorYml: false,
+      enableSecureVariablesInPullRequests: false,
+      enableSecureVariablesInPullRequestsFromSameRepo: false,
+      enableDeploymentInPullRequests: false,
+      rollingBuilds: false,
+      alwaysBuildClosedPullRequests: false,
+      tags: '',
+      nuGetFeed: {
+        id: 'test-proj-ikb41f0xwpjv',
+        name: 'Project test-proj',
+        accountId: 6789,
+        projectId: 12345,
+        publishingEnabled: false,
+        created: '2016-09-21T00:08:26.9522633+00:00'
+      },
+      securityDescriptor: {
+        accessRightDefinitions: [
+          {
+            name: 'View',
+            description: 'View'
+          },
+          {
+            name: 'RunBuild',
+            description: 'Run build'
+          },
+          {
+            name: 'Update',
+            description: 'Update settings'
+          },
+          {
+            name: 'Delete',
+            description: 'Delete project'
+          }
+        ],
+        roleAces: [
+          {
+            roleId: 11111,
+            name: 'Administrator',
+            isAdmin: true,
+            accessRights: [
+              {
+                name: 'View',
+                allowed: true
+              },
+              {
+                name: 'RunBuild',
+                allowed: true
+              },
+              {
+                name: 'Update',
+                allowed: true
+              },
+              {
+                name: 'Delete',
+                allowed: true
+              }
+            ]
+          },
+          {
+            roleId: 22222,
+            name: 'User',
+            isAdmin: false,
+            accessRights: [
+              {
+                name: 'View'
+              },
+              {
+                name: 'RunBuild'
+              },
+              {
+                name: 'Update'
+              },
+              {
+                name: 'Delete'
+              }
+            ]
+          }
+        ]
+      },
+      created: '2016-09-21T00:08:25.2289648+00:00',
+      updated: '2016-09-21T00:29:31.1506705+00:00'
+    },
+    build: {
+      buildId: 9876543,
+      jobs: [
+        {
+          jobId: 'xu2z04xk4xjdtm3i',
+          name: 'Platform: x86',
+          allowFailure: false,
+          messagesCount: 0,
+          compilationMessagesCount: 0,
+          compilationErrorsCount: 0,
+          compilationWarningsCount: 0,
+          testsCount: 0,
+          passedTestsCount: 0,
+          failedTestsCount: 0,
+          artifactsCount: 0,
+          status: status,
+          started: '2016-11-16T20:42:09.1953605+00:00',
+          finished: '2016-11-16T20:42:32.2101666+00:00',
+          created: '2016-11-16T20:42:01.183924+00:00',
+          updated: '2016-11-16T20:42:32.2257992+00:00'
+        },
+        {
+          jobId: 'u4c732mxowrfkwaq',
+          name: 'Platform: amd64',
+          allowFailure: false,
+          messagesCount: 0,
+          compilationMessagesCount: 0,
+          compilationErrorsCount: 0,
+          compilationWarningsCount: 0,
+          testsCount: 0,
+          passedTestsCount: 0,
+          failedTestsCount: 0,
+          artifactsCount: 0,
+          status: status,
+          started: '2016-11-16T20:42:38.1432759+00:00',
+          finished: '2016-11-16T20:42:59.2526017+00:00',
+          created: '2016-11-16T20:42:01.5433296+00:00',
+          updated: '2016-11-16T20:42:59.2682185+00:00'
+        }
+      ],
+      buildNumber: 63,
+      version: '0.0.63',
+      message: 'test commit messages',
+      messageExtended: 'test commit extended message',
+      branch: 'master',
+      isTag: false,
+      commitId: '123098123a941928301820ef938ab2c123572909',
+      authorName: 'Test Author',
+      authorUsername: 'test-author-user',
+      committerName: 'Test Committer',
+      committerUsername: 'test-committer-user',
+      committed: '2016-11-16T20:38:38+00:00',
+      messages: [],
+      status: status,
+      started: '2016-11-16T20:42:09.2109847+00:00',
+      finished: '2016-11-16T20:42:59.486954+00:00',
+      created: '2016-11-16T20:41:59.1683638+00:00',
+      updated: '2016-11-16T20:42:59.486954+00:00'
+    }
+  };
+};
+
+exports.getStatusBadge = function getStatusBadge(status) {
+  if (!badgeData) {
+    var badgeDataPath = path.join(__dirname, '..', 'test-data', 'badge.svg');
+    badgeData =
+      fs.readFileSync(badgeDataPath, {encoding: 'utf-8'});
+  }
+
+  return badgeData.replace(/\bpassing\b/g, status);
+};
---
+++ test/lib/appveyor-utils.js
@@ -0,0 +1,409 @@
+/**
+ * @copyright Copyright 2017 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+'use strict';
+
+var apiResponses = require('../../test-lib/api-responses');
+var appveyorUtils = require('../../lib/appveyor-utils');
+var assert = require('assert');
+
+var deepStrictEqual = assert.deepStrictEqual || assert.deepEqual;
+
+describe('appveyorUtils', function() {
+  describe('.badgeToStatus', function() {
+    ['success', 'failed'].forEach(function(status) {
+      it('extracts ' + status + ' status', function() {
+        var badge = apiResponses.getStatusBadge(status);
+        var result = appveyorUtils.badgeToStatus(badge);
+        assert.strictEqual(result, status);
+      });
+    });
+
+    it('throws for unrecognized status', function() {
+      var badge = apiResponses.getStatusBadge('whatever');
+      assert.throws(
+        function() { appveyorUtils.badgeToStatus(badge); },
+        Error
+      );
+    });
+
+    it('throws for ambiguous status', function() {
+      var badge = apiResponses.getStatusBadge('success failed');
+      assert.throws(
+        function() { appveyorUtils.badgeToStatus(badge); },
+        Error
+      );
+    });
+
+    it('throws for non-string', function() {
+      assert.throws(
+        function() { appveyorUtils.badgeToStatus(null); },
+        Error
+      );
+    });
+  });
+
+  describe('.projectBuildToStatus', function() {
+    it('returns any status of ProjectBuild', function() {
+      var testStatus = 'foo';
+      var projectBuild = apiResponses.getProjectBuild(testStatus);
+      var result = appveyorUtils.projectBuildToStatus(projectBuild);
+      assert.strictEqual(result, testStatus);
+    });
+  });
+
+  describe('.parseAppveyorRepoUrl', function() {
+    ['mercurial', 'subversion'].forEach(function(testScm) {
+      it('returns ' + testScm + ' URL as-is', function() {
+        var testUrl = 'foo';
+        deepStrictEqual(
+          appveyorUtils.parseAppveyorRepoUrl(testScm, testUrl),
+          {
+            repositoryScm: testScm,
+            repositoryType: testScm,
+            repositoryName: testUrl
+          }
+        );
+      });
+    });
+
+    it('parses bitBucket HTTPS URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'https://bitbucket.org/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'bitBucket',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    it('parses bitBucket SSH URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'git@bitbucket.org:' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'bitBucket',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    it('parses gitHub HTTPS URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'https://github.com/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'gitHub',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    it('parses gitHub SSH URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'git@github.com:' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'gitHub',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    it('parses gitLab HTTPS URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'https://gitlab.com/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'gitLab',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    it('parses gitLab SSH URL', function() {
+      var testProject = 'foo/bar';
+      var testUrl = 'git@gitlab.com:' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'gitLab',
+          repositoryName: testProject
+        }
+      );
+    });
+
+    // FIXME:  Can't be sure this works without paid AppVeyor account
+    it('parses vso project git HTTPS URL', function() {
+      var testUrl = 'https://kevinoid.visualstudio.com/_git/TestProj';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'vso',
+          repositoryName: 'git/kevinoid/TestProj/TestProj'
+        }
+      );
+    });
+
+    // FIXME:  Can't be sure this works without paid AppVeyor account
+    it('parses vso project git SSH URL', function() {
+      var testUrl = 'ssh://kevinoid@kevinoid.visualstudio.com:22/_git/TestProj';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'vso',
+          repositoryName: 'git/kevinoid/TestProj/TestProj'
+        }
+      );
+    });
+
+    // FIXME:  Can't be sure this works without paid AppVeyor account
+    it('parses vso sub-project git HTTPS URL', function() {
+      var testUrl = 'https://kevinoid.visualstudio.com/TestProj/_git/repo2';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'vso',
+          repositoryName: 'git/kevinoid/TestProj/repo2'
+        }
+      );
+    });
+
+    // FIXME:  Can't be sure this works without paid AppVeyor account
+    it('parses vso sub-project git SSH URL', function() {
+      var testUrl =
+        'ssh://kevinoid@kevinoid.visualstudio.com:22/TestProj/_git/repo2';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'vso',
+          repositoryName: 'git/kevinoid/TestProj/repo2'
+        }
+      );
+    });
+
+    it('returns unknown git HTTPS URL', function() {
+      var testUrl = 'https://example.com/foo.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'git',
+          repositoryName: testUrl
+        }
+      );
+    });
+
+    it('returns unknown git SSH URL', function() {
+      var testUrl = 'user@example.com:foo.git';
+      deepStrictEqual(
+        appveyorUtils.parseAppveyorRepoUrl('git', testUrl),
+        {
+          repositoryScm: 'git',
+          repositoryType: 'git',
+          repositoryName: testUrl
+        }
+      );
+    });
+  });
+
+  describe('.repoUrlToBadgeParams', function() {
+    ['mercurial', 'subversion'].forEach(function(testScm) {
+      it('throws Error for ' + testScm + ' scm', function() {
+        var testUrl = 'foo';
+        assert.throws(
+          function() { appveyorUtils.repoUrlToBadgeParams(testScm, testUrl); },
+          Error
+        );
+      });
+    });
+
+    it('parses bitBucket HTTPS URL', function() {
+      var testAccount = 'foo';
+      var testProject = 'bar';
+      var testUrl =
+        'https://bitbucket.org/' + testAccount + '/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.repoUrlToBadgeParams('git', testUrl),
+        {
+          badgeRepoProvider: 'bitBucket',
+          repoAccountName: testAccount,
+          repoSlug: testProject
+        }
+      );
+    });
+
+    it('parses bitBucket SSH URL', function() {
+      var testAccount = 'foo';
+      var testProject = 'bar';
+      var testUrl =
+        'git@bitbucket.org:' + testAccount + '/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.repoUrlToBadgeParams('git', testUrl),
+        {
+          badgeRepoProvider: 'bitBucket',
+          repoAccountName: testAccount,
+          repoSlug: testProject
+        }
+      );
+    });
+
+    // As far as I know this can not happen.  If it can, figure out how to
+    // support this.
+    it('throws for bitBucket URL with 3 path parts', function() {
+      var testUrl = 'https://bitbucket.org/foo/bar/baz.git';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    it('parses gitHub HTTPS URL', function() {
+      var testAccount = 'foo';
+      var testProject = 'bar';
+      var testUrl =
+        'https://github.com/' + testAccount + '/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.repoUrlToBadgeParams('git', testUrl),
+        {
+          badgeRepoProvider: 'gitHub',
+          repoAccountName: testAccount,
+          repoSlug: testProject
+        }
+      );
+    });
+
+    it('parses gitHub SSH URL', function() {
+      var testAccount = 'foo';
+      var testProject = 'bar';
+      var testUrl =
+        'git@github.com:' + testAccount + '/' + testProject + '.git';
+      deepStrictEqual(
+        appveyorUtils.repoUrlToBadgeParams('git', testUrl),
+        {
+          badgeRepoProvider: 'gitHub',
+          repoAccountName: testAccount,
+          repoSlug: testProject
+        }
+      );
+    });
+
+    // Not supported by AppVeyor
+    it('throws for gitLab HTTPS URL', function() {
+      var testUrl = 'https://gitlab.com/foo/bar.git';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    // Not supported by AppVeyor
+    it('throws for gitLab HTTPS URL', function() {
+      var testUrl = 'git@gitlab.com:foo/bar.git';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    // Not supported by AppVeyor
+    it('throws for vso HTTPS URL', function() {
+      var testUrl = 'https://kevinoid.visualstudio.com/_git/TestProj';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    // Not supported by AppVeyor
+    it('throws for vso SSH URL', function() {
+      var testUrl = 'ssh://kevinoid@kevinoid.visualstudio.com:22/_git/TestProj';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    it('throws for other git HTTPS URLs', function() {
+      var testUrl = 'https://example.com/foo.git';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+
+    it('returns unknown git SSH URL', function() {
+      var testUrl = 'user@example.com:foo.git';
+      assert.throws(
+        function() { appveyorUtils.repoUrlToBadgeParams('git', testUrl); },
+        Error
+      );
+    });
+  });
+
+  describe('.projectFromString', function() {
+    it('splits account name and slug to object', function() {
+      var accountName = 'foo';
+      var slug = 'bar';
+      deepStrictEqual(
+        appveyorUtils.projectFromString(accountName + '/' + slug),
+        {
+          accountName: accountName,
+          slug: slug
+        }
+      );
+    });
+
+    it('throws for string with 1 path part', function() {
+      assert.throws(
+        function() { appveyorUtils.projectFromString('foo'); },
+        Error
+      );
+    });
+
+    it('throws for string with 3 path part', function() {
+      assert.throws(
+        function() { appveyorUtils.projectFromString('foo/bar/baz'); },
+        Error
+      );
+    });
+
+    it('throws for non-string', function() {
+      assert.throws(
+        function() { appveyorUtils.projectFromString(null); },
+        Error
+      );
+    });
+  });
+
+  describe('.projectToString', function() {
+    it('joins account name and slug', function() {
+      var testProj = {
+        accountName: 'foo',
+        slug: 'bar'
+      };
+      deepStrictEqual(
+        appveyorUtils.projectToString(testProj),
+        'foo/bar'
+      );
+    });
+  });
+});
